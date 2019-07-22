cd512d16097800d9d16e36c7b46ebe01fab9302a
Leonardo Oliveira <leonfoliveira@gmail.com>
2019-07-22 17:17:52 -0300

    Alignment corrections

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import logo_icpc from './img/logo_icpc.png';
 import logo_mdp from './img/logo_mdp.jpg';
 import logo_mineira from './img/logo_mineira.png';
 import logo_b2ml from './img/logo_b2ml.png';
+import logo_unifei from './img/logo_unifei.png';
 
 let about = "A competição de programação, é uma prova de 5 horas, com 8 a 14 problemas para serem resolvidos. Existem 4 competições de programação que a UNIFEI vem participando. A principal é a Maratona de Programação. Ela é dividida em 3 fases no Brasil. A primeira é uma competição regional, realizada sempre em setembro de cada ano, em uma sede em cidade próxima a Itajubá. Essa etapa, através das regras criadas pela organização, classifica para a fase final brasileira ou regional da América Latina. As equipes que passarem na primeira fase, geralmente em torno de 60 a 70 equipes do Brasil inteiro, se reúnem em novembro em uma cidade pré-definida para disputarem uma das 5 ou 6 vagas para a etapa Mundial. O Mundial é em um país e cidade pré-definida e ocorre em maio do ano seguinte.";
 
@@ -51,20 +52,26 @@ class App extends React.Component {
 
         <div className="jumbotron" style={{backgroundImage: `url(${background_icpc})`}}>
           <div className="shadow">
-            <img src={logo_epic} alt="Logo" height='300'></img>
+            <img src={logo_epic} alt="Logo EPIC" height='300' style={{zIndex: 2}}></img>
           </div>
         </div>
         <section className="text-center" id="sec0">
-          <div className="img-box">
-            <a href="https://icpc.baylor.edu/">
-              <img src={logo_icpc} alt="Logo ICPC" height='100'></img>
-            </a>
-            <a href="http://maratona.ime.usp.br/">
-              <img src={logo_mdp} alt="Logo Maratona de Programação" height='150'></img>
-            </a>
-            <a href="https://maratona.algartelecom.com.br/portal/mineira/">
-              <img src={logo_mineira} alt="Logo Mineira de Programação" height='150'></img>
-            </a>
+          <div className="row img-box">
+            <div className="col-lg text-right">
+              <a href="https://icpc.baylor.edu/">
+                <img src={logo_icpc} alt="Logo ICPC" height='125' style={{marginTop: 12}}></img>
+              </a>
+            </div>
+            <div className="col-lg">
+              <a href="http://maratona.ime.usp.br/">
+                <img src={logo_mdp} alt="Logo Maratona de Programação" height='150'></img>
+              </a>
+            </div>
+            <div className="col-lg text-left">
+              <a href="https://maratona.algartelecom.com.br/portal/mineira/">
+                <img src={logo_mineira} alt="Logo Mineira de Programação" height='150'></img>
+              </a>
+            </div>
           </div>
         </section>
         <section className="dark text-center" id="sec1">
