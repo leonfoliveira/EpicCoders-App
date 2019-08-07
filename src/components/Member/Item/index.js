0f7ce9169246033b4b004ad1c5fc4407ccfb5a98
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-08-07 13:45:02 -0300

    Refactory MemberItem component

--- src/components/Member/Item/index.js
+++ src/components/Member/Item/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 import image from './img/user.jpg';
 
-const MemberItem = props => (
+const MemberItem = ({ name, team }) => (
   <div className="col-sm-4 text-center">
     <div className="member">
-      <img src={image} alt={props.name}></img>
-      <h5>{props.name}</h5>
-      <h3>{props.team}</h3>
+      <img src={image} alt={name}></img>
+      <h5>{name}</h5>
+      <h3>{team}</h3>
     </div>
   </div>
 )
