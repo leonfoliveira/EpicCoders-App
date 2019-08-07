8ea7970972c8c61aef06e4a6a1014ca254ed1cf1
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2019-08-07 13:31:22 -0300

    Refactory EventItem component
    Static components is more reable written in functions
    over class.

--- src/components/Event/Item/index.js
+++ src/components/Event/Item/index.js
@@ -1,27 +1,18 @@
 import React from "react"
 
-class EventItem extends React.Component {
-  constructor(props) {
-    super(props);
+function EventItem({ date, desc }) {
+  const normalizedDate = new Date(
+    date.split('/')[1]+'-'+
+    date.split('/')[0]+'-'+
+    date.split('/')[2]
+  )
 
-    this.state = {
-      date: new Date(
-        props.date.split('/')[1]+'-'+
-        props.date.split('/')[0]+'-'+
-        props.date.split('/')[2]
-      )
-    }
-  }
-
-  render() {
-    console.log(this.state.date);
-    return (
-      <tr className={this.state.date < new Date() ? 'passed' : 'active'}>
-        <th>{this.props.date}</th>
-        <th>{this.props.desc}</th>
-      </tr>
-    );
-  }
+  return (
+    <tr className={normalizedDate < new Date() ? 'passed' : 'active'}>
+      <th>{date}</th>
+      <th>{desc}</th>
+    </tr>
+  );
 }
 
 export default EventItem
