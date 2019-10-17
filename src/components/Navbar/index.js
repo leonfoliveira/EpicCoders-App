4d3978f4a306de3c8e9002625b945b6400cca3cd
Leonardo F Oliveira <leonfoliveira@gmail.com>
2019-10-17 16:45:49 -0300

    Navbar refactored

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,60 +1,46 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 
 import NavbarLink from "./Link"
-import letras_epic from './letras_epic.png'
-import logo_unifei from './logo_unifei.png';
+import letras_epic from '../../img/letras_epic.png'
+import logo_unifei from '../../img/logo_unifei.png';
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Navbar () {
 
-    this.state = {
-      visible: false
-    }
+  const [ visible, setVisible ] = useState(false);
 
-    this.handleScroll = this.handleScroll.bind(this);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    setVisible(window.pageYOffset > 10);
   }
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
+  return (
+    <nav className={"navbar navbar-expand-md fixed-top "+(visible ? "active" : "hidden")} id="navbar-fixed">
+      <div className="navbar-brand">
+        <img src={letras_epic} alt="EPIC" height='20'></img>
+      </div>
+      <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll() {
-    this.setState({
-      visible: window.pageYOffset > 10
-    });
-  }
-
-  render() {
-    return (
-      <nav className={"navbar navbar-expand-md fixed-top "+(this.state.visible ? "active" : "hidden")} id="navbar-fixed">
-        <div className="navbar-brand">
-          <img src={letras_epic} alt="EPIC" height='20'></img>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="navbar-nav">
+          <NavbarLink sec="navbar-nofixed" text="Home"></NavbarLink>
+          <NavbarLink sec="sec1" text="Sobre"></NavbarLink>
+          <NavbarLink sec="sec2" text="Equipes"></NavbarLink>
+          <NavbarLink sec="sec3" text="Calendário"></NavbarLink>
+          <NavbarLink sec="sec4" text="Patrocínio"></NavbarLink>
         </div>
-        <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span className="navbar-toggler-icon"></span>
-        </button>
+      </div>
 
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-            <div className="navbar-nav">
-                <NavbarLink sec="navbar-nofixed" text="Home"></NavbarLink>
-                <NavbarLink sec="sec1" text="Sobre"></NavbarLink>
-                <NavbarLink sec="sec2" text="Equipes"></NavbarLink>
-                <NavbarLink sec="sec3" text="Calendário"></NavbarLink>
-                <NavbarLink sec="sec4" text="Patrocínio"></NavbarLink>
-            </div>
-        </div>
-
-        <a href="https://unifei.edu.br" target="_blank" rel="noopener noreferrer">
-          <img src={logo_unifei} alt="Logo UNIFEI" height='92' id="logo-unifei"></img>
-        </a>
-      </nav>
-    )
-  }
+      <a href="https://unifei.edu.br" target="_blank" rel="noopener noreferrer">
+        <img src={logo_unifei} alt="Logo UNIFEI" height='92' id="logo-unifei"></img>
+      </a>
+    </nav>
+  )
 }
-
-export default Navbar;
