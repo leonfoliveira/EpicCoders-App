938ec07340e0b2ef53bdfe099cb5f512d8200ecf
Leonardo F Oliveira <leonfoliveira@gmail.com>
2019-10-17 18:48:07 -0300

    Normalize date fixed

--- src/components/Calendar/Item/index.js
+++ src/components/Calendar/Item/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function CalendarItem ({ date, today, desc }) {
   const normalizedDate = new Date(
+    date.split('/')[2]+'-'+
     date.split('/')[1]+'-'+
-    date.split('/')[0]+'-'+
-    date.split('/')[2]
+    date.split('/')[0]
   )
 
   return (
